6bb01db5c3fc1f052a293a8382b7891dba936da1
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-09-03 23:03:58 +0200

    update: name of arguments

--- arguments.py
+++ arguments.py
@@ -12,7 +12,7 @@ def get_args():
 	# parser.add_argument('--data_path', type=str, default='./../data', help='Path to where the data is')
 	# parser.add_argument('--out_path', type=str, default='./../results', help='Path to where the output log will be')
 	# TODO: path to save model
-	parser.add_argument('--max_length', type=int, default=384, help='Max length of each sequence')
+	parser.add_argument('--max_length', type=int, default=None, help='Max length of each sequence')
 	# parser.add_argument('--log_name', type=str, default='test.log', help='middle outputs')
 	#parser.add_argument('--help', '-h', default=False, action='store_true', help='verbose')
 	parser.add_argument('--cuda', action='store_true', help='If training is to be done on a GPU')
@@ -22,7 +22,7 @@ def get_args():
 	parser.add_argument('--seed', '-e', default=1127, type=int, help='random seed')
 	# new args
 	parser.add_argument('--model_batch', '-c', default=8, type=int, help='batch size for training the model')
-	parser.add_argument('--before_exp', '-x', default=False, type=bool, help='True if it runs for development with small set of data.')
+	parser.add_argument('--toy_exp', '-x', default=False, type=bool, help='True if it runs for development with small set of data.')
 	parser.add_argument('--learning_rate', '-l', default=1e-4, type=float, help='learning rate for training')
 	# lpl
 	# parser.add_argument('--lpl_epoches', type=int, default=20, help='lpl epoch num after detach')
