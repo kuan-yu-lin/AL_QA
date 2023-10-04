241763b099a6ed1d1b57c8c2d41e8d7fa585f3c2
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-10-04 23:45:16 +0200

    experiments scripts in unique context setting

--- experiment/unique_context/uc_lr.sh
+++ experiment/unique_context/uc_lr.sh
@@ -1,109 +1,109 @@
 #!/bin/bash
 
-python main_lowRes_new.py \
+python main.py \
     -a RandomSampling \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a KMeansSampling \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a KCenterGreedy \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a BALDDropout \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a MeanSTD \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a BadgeSampling \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a MarginSampling \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a LeastConfidence \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a EntropySampling \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a MarginSamplingDropout \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a LeastConfidenceDropout \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
 
-python main_lowRes_new.py \
+python main.py \
     -a EntropySamplingDropout \
     -q 200 \
     -b 50 \
-    -d SearchQA \
+    -d BioASQ \
     -u True \
     -r True \
-    -g 3
+    -g 7
